c07c899deca5e2e2e9b46c7acbc3219bdca1cabc
Arifin <ar.wijaya1221@gmail.com>
2023-12-17 03:15:57 +0700

    memperbaiki penguncian halaman atau route, dan mengubah tampilannya

--- app/Http/Middleware/CheckSiteStatus.php
+++ app/Http/Middleware/CheckSiteStatus.php
@@ -4,19 +4,20 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Support\Facades\View;
 
 class CheckSiteStatus
 {
     public function handle($request, Closure $next)
     {
-        $activationDate = strtotime('2024-1-1'); // Gantilah dengan tanggal aktivasi yang sesuai
+        $activationDate = strtotime('2030-01-01'); // Gantilah dengan tanggal aktivasi yang sesuai
         $inactiveDays = 10;
 
         if (time() - $activationDate > $inactiveDays * 24 * 60 * 60) {
-            return response("Website is currently inactive. Please contact support.", 403);
+            // Return a custom view for inactive website
+            return response(View::make('inactive'), 403);
         }
 
         return $next($request);
     }
 }
-
--- routes/web.php
+++ routes/web.php
@@ -19,77 +19,25 @@ use App\Http\Controllers\ProfileController;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
-Route::get('/', [HomeController::class, 'index']);
-
-Route::get('auth', [AuthController::class, 'index']);
-Route::resource('profile', ProfileController::class)->middleware('web');
-Route::get('/login', [AuthController::class, 'index'])->name('login');
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
-
-
-
-Route::get('code', [HomeController::class, 'code']);
-Route::put('/code/update/{id}', [HomeController::class, 'update'])->name('code.update');
-Route::get('/code/{id}', [HomeController::class, 'show']);
-Route::get('/live-demo/{id}', [HomeController::class, 'demo']);
-
-Route::get('admin', [AdminController::class, 'index']);
-Route::resource('admin/akun', AkunController::class)->middleware('web');
-Route::resource('admin/template', TemplateController::class)->middleware('web');
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 Route::group(['middleware' => 'check.site.status'], function () {
-    Route::get('admin', [AdminController::class, 'index']);
-    Route::resource('admin/akun', AkunController::class)->middleware('web');
-    Route::resource('admin/template', TemplateController::class)->middleware('web');
+    Route::get('/', [HomeController::class, 'index']);
+    Route::get('code', [HomeController::class, 'code']);
+    Route::put('/code/update/{id}', [HomeController::class, 'update'])->name('code.update');
+    Route::get('/code/{id}', [HomeController::class, 'show']);
+    Route::get('/live-demo/{id}', [HomeController::class, 'demo']);
+    Route::middleware(['auth'])->group(function () {
+        Route::resource('profile', ProfileController::class);
+        Route::middleware(['auth'])->group(function () {
+            Route::get('admin', [AdminController::class, 'index']);
+            Route::resource('admin/akun', AkunController::class);
+            Route::resource('admin/template', TemplateController::class);
+        });
+    });
+
+
+    Route::get('/login', [AuthController::class, 'index'])->name('login');
+    Route::get('/auth', [AuthController::class, 'index']);
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 });
+
